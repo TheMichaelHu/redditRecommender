12078e5c4282d832be68654de17ebc152239aea2
Michael <michaelanhu@gmail.com>
2015-08-04 20:14:20 -0400

    data gathering code complete

--- recommend.py
+++ recommend.py
@@ -5,9 +5,8 @@
 # ---------------
 
 # Checklist of things to implement:
-# pull 100 users from comments					DONE
-# pull 100 comments from each user
-# pull subreddits from comments
+# pull 100 unique users from comments						DONE
+# pull 100 subreddits from comments from each user 			DONE
 # create similarity function
 # write similarity to database 
 # sort similarity and output recommendation
@@ -17,39 +16,76 @@ import json
 import urllib2
 
 USR_AG = {'User-Agent' : 'Arbitrary User Agent Name'}
+NUM_USERS = 100
+NUM_SUBREDDITS = 100
 
 def main():
 	recommend('gameofthrones')
 
+# Takes subreddit, gives recommendation
 def recommend(subreddit):
-	r = requests.get('http://www.reddit.com/r/'+subreddit+'/comments/.json', headers = USR_AG)
-	data = r.json()
+	print getSubreddits(subreddit)
 
-	users = getUsers(data, 100)
-	for user in users:
-		print user
-
-def getNextPage(data):
-	subreddit = data['data']['children'][0]['data']['subreddit']
-	after = data['data']['after']
-	r = requests.get('http://www.reddit.com/r/'+subreddit+'/comments/.json?after='+after, headers = USR_AG)
+# Gets JSON for page after the given one
+def getNextPage(url, after):
+	if after is None:
+		return None
+	r = requests.get(url+'?after='+after, headers = USR_AG)
 	return r.json()
 
-def getUsers(data, numUsers):
-	pageData = data
+# Get up to n unique users from given JSON.
+def getUsers(subreddit, numUsers):
+	r = requests.get('http://www.reddit.com/r/'+subreddit+'/comments/.json', headers = USR_AG)
+	data = r.json()
 	users = []
 
 	index = 0
 	while(len(users) < numUsers):
-		if(len(pageData['data']['children']) > index):
-			user = pageData['data']['children'][index]['data']['author']
+		if(len(data['data']['children']) > index):
+			user = data['data']['children'][index]['data']['author']
 			if user not in users:
 				users.append(user)
 			index += 1
 		else:
-			pageData = getNextPage(pageData)
+			data = getNextPage('http://www.reddit.com/r/'+subreddit+'/comments/.json', data['data']['after'])
 			index = 0
+			if data is None:
+				break
 
 	return users
+
+# Get up to latest n comments from given user
+def getSubredditsForUser(user, numSubreddits):
+	r = requests.get('http://www.reddit.com/user/'+user+'/comments/.json', headers = USR_AG)
+	data = r.json()
+	subreddits = []
+
+	index = 0
+	while(len(subreddits) < numSubreddits):
+		if(len(data['data']['children']) > index):
+			subreddits.append(data['data']['children'][index]['data']['subreddit'])
+			index += 1
+		else:
+			data = getNextPage('http://www.reddit.com/user/'+user+'/comments/.json', data['data']['after'])
+			index = 0
+			if data is None:
+				break
+
+	return subreddits
+
+# Get subreddits recent commenters have also been to
+def getSubreddits(subreddit):
+	subreddits = {}
+	users = getUsers(subreddit, NUM_USERS)
+
+	for user in users:
+		user_subreddits = getSubredditsForUser(user, NUM_SUBREDDITS)
+		for subreddit in user_subreddits:
+			if subreddit in subreddits:
+				subreddits[subreddit] += 1
+			else:
+				subreddits[subreddit] = 1 
+
+	return subreddits
 
 main()
