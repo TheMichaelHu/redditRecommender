c1aade814b014f64a009862e0e35fb1455c07fce
luggaboom <yvettekim92@gmail.com>
2015-08-10 14:42:08 -0400

    adding database functions

--- recommend.py
+++ recommend.py
@@ -8,8 +8,8 @@
 # pull 100 unique users from comments						DONE
 # pull 100 subreddits from comments from each user 			DONE
 # create similarity function								DONE
-# write similarity to database 
-# sort similarity and output recommendation
+# write similarity to database 								DONE
+# sort similarity and output recommendation					DONE
 
 import requests
 import json
@@ -20,6 +20,7 @@ import urlparse
 USR_AG = {'User-Agent' : 'Arbitrary User Agent Name'}
 NUM_USERS = 100
 NUM_SUBREDDITS = 100
+SUBREDDITS = 
 
 def main():
 	recommend('gameofthrones')
@@ -133,11 +134,72 @@ def cosine_similiarity(subreddit1, subreddit2):
 
 # Write the similarity of two subreddits to the database
 def write_subreddit_similarity(subreddit1, subreddit2):
-	#Stub
+	#assuming the similarity will be the same for (subreddit1,subreddit2) and (subreddit2,subreddit1), we insert twice 
+	urlparse.uses_netloc.append("postgres")
+	url = urlparse.urlparse("postgres://tzxzlthahxikmb:4BNoR1mJxoFquJtf0X322KTLO8@ec2-54-83-51-0.compute-1.amazonaws.com:5432/dbknaoq86ntmdo")
+
+	conn = psycopg2.connect(
+		database=url.path[1:],
+		user=url.username,
+		password=url.password,
+		host=url.hostname,
+		port=url.port
+	)
+
+	cur = conn.cursor()
+
+	value = cosine_similiarity(subreddit1, subreddit2)
+	SQL = "INSERT INTO ratings(%(reddit1)s, %(reddit2)s) VALUES (%(val)s);"
+	data1 = ({'reddit1': subreddit1, 'reddit2':subreddit2, 'val': value})
+	data2 = ({'reddit1': subreddit2, 'reddit2':subreddit1, 'val': value})
+
+	print(cur.mogrify(SQL, data1))
+	print(cur.mogrify(SQL, data2))
+
 	return None
 
+# get subreddit similarity for these two subreddits
 def read_subreddit_similarity(subreddit1, subreddit2):
-	#Stub
+	urlparse.uses_netloc.append("postgres")
+	url = urlparse.urlparse("postgres://tzxzlthahxikmb:4BNoR1mJxoFquJtf0X322KTLO8@ec2-54-83-51-0.compute-1.amazonaws.com:5432/dbknaoq86ntmdo")
+
+	conn = psycopg2.connect(
+		database=url.path[1:],
+		user=url.username,
+		password=url.password,
+		host=url.hostname,
+		port=url.port
+	)
+
+	cur = conn.cursor()
+
+	SQL = "SELECT %(reddit1)s FROM ratings WHERE subreddit = %(reddit2)s;"
+	data = ({'reddit1': subreddit1, 'reddit2':subreddit2})
+	print(cur.mogrify(SQL, data))
+	cur.execute(SQL, data)
+	print(cur.fetchall)
+	
 	return None
+
+def get_subreddit_recommendation(subreddit, num_of_results):
+	urlparse.uses_netloc.append("postgres")
+	url = urlparse.urlparse("postgres://tzxzlthahxikmb:4BNoR1mJxoFquJtf0X322KTLO8@ec2-54-83-51-0.compute-1.amazonaws.com:5432/dbknaoq86ntmdo")
+
+	conn = psycopg2.connect(
+		database=url.path[1:],
+		user=url.username,
+		password=url.password,
+		host=url.hostname,
+		port=url.port
+	)
+	
+	cur = conn.cursor()
+
+	# this is broken down this way because postgresql ORDER BY is dumb and can't handle placeholder processing
+	SQL = "SELECT reddit FROM ratings ORDER BY %s DESC LIMIT %%(num)s;" % subreddit
+	data = ({"num":num_of_results})
+		  
+	cur.execute(SQL, data)
+	print cur.fetchall()
 
 main()
